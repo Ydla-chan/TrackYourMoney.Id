cb9508700aa733bf6b1fd464cebabebaeb1398ba
Tyasweningas <141199272+Tyasweningas@users.noreply.github.com>
2023-12-14 12:36:31 +0700

    Landing Page
    Uploading Landing Page and Finished Landing For Landing Page

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+  <!-- Head info -->
+  <title>Track Your Money</title>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+ 
+  <!-- Logo Icon Tab  -->
+  <link rel="icon" type="image/x-icon" href="./assets/img/loco.jpg">
+
+  <!-- Google Fonts -->
+  <linkhref="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- External CSS-->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- CSS internal  -->
+  <link href="assets/css/style.css" rel="stylesheet">
+ </head>
+<body>
+
+  <!-- ======= Navbar ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div class="logo">
+      <h1><a href="index.html"><span>Track Your Money</span></a></h1>
+      </div>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hlmnutm">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#fitur">Fitur </a></li>
+          <li><a class="nav-link scrollto" href="#Part">Kerja Sama</a></li>
+          <li><a class="nav-link scrollto" href="#faq">Pertanyaan</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Kontak Kami</a></li>
+          <li><a class="nav-link scrollto" href="./view/masuk-akun.php">Masuk</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+
+  <!-- ======= Halaman Utama ======= -->
+  <section id="hlmnutm">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+          <div data-aos="zoom-out">
+            <h1>Selamat Datang Di <br><span> Track Your Money</span></h1>
+            <h2>“senantiasa membantu anda dengan catatan laporan terbaik”</h2>
+            <div class="text-center text-lg-start">
+              <a href="#fitur" class="btn-get-started scrollto">Jelajahi</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2 hlmnutm-img" data-aos="zoom-out" data-aos-delay="300">
+          <img src="./assets/img/gambarruang.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+    <svg class="hlmnutm-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="#91B278">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(0,153,0, .1)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#679147">
+      </g>
+    </svg>
+  </section>
+
+
+  <!-- Halaman hyperlinks  -->
+  <main id="main">
+
+    <!-- ======= Div Fitur ======= -->
+    <section id="fitur" class="fitur">
+      <div class="container">
+        <div class="section-title" data-aos="fade-up">
+          <h2>Fitur</h2>
+          <p>Fitur Yang tersedia dalam sistem ini </p>
+        </div>
+        <div class="row" data-aos="fade-left">
+          <div class="col-lg-3 col-md-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="50">
+              <i class="ri-file-list-3-fill" style="color: #ffbb2c;"></i>
+              <h3>Mencatat Pengeluaran Harian </h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+              <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+              <h3><a href="./login.php">Melihat Grafik Pengeluaran</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="150">
+              <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
+              <h3><a href="./login.php">Membuat Pengingat Bulanan</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+              <i class="ri-file-list-3-fill" style="color: #e361ff;"></i>
+              <h3><a href="./login.php">Melihat Laporan Pengeluaran</a></h3>
+            </div>
+          </div>
+        </div>
+    </section>
+
+   <!-- ======= Div Pemegang saham ======= -->  
+    <section id="Part" class="Part">
+      <div class="container">
+        <div class="section-title" data-aos="fade-up">
+          <h2>Pemegang Saham</h2>
+          <p>Pemegang Saham Proyek</p>
+        </div>
+          <div class="row" data-aos="fade-left">
+            <div class="col-lg-4 col-md-6">
+              <div class="member" data-aos="zoom-in" data-aos-delay="100">
+                <div class="pic"><img src="assets/img/ParnertShip/poli.png" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4 class="univ">Universitas Politeknik Negeri Batam</h4>
+                  <span>Universitas</span>
+                  <div class="social">
+                    <a href="https://www.polibatam.ac.id/"><i class="bi bi-globe2 "></i></a>
+                    <a href="https://www.facebook.com/polibatamofficial"><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.instagram.com/polibatamofficial/"><i class="bi bi-instagram"></i></a>
+                    <a href="https://www.linkedin.com/school/politeknik-negeri-batam/"><i class="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
+            <div class="member" data-aos="zoom-in" data-aos-delay="200">
+              <div class="pic"><img src="./assets/img/ParnertShip/informatika.png" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4 class="univ">Teknik Informatika </h4>
+                <span>Jurusan</span>
+                <div class="social">
+                     <a href="https://if.polibatam.ac.id/"><i class="bi bi-globe2 "></i></a>
+                    <a href="https://www.instagram.com/jur_if.polibatam/"><i class="bi bi-instagram"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
+            <div class="member" data-aos="zoom-in" data-aos-delay="300">
+              <div class="pic"><img src="./assets/img/ParnertShip/LOGO_Prod_TRPL_Variant_09_Square Color.png" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Teknologi Rekayasa Perangkat Lunak</h4>
+                <span>Program Studi</span>
+                <div class="social">
+                    <a href="https://if.polibatam.ac.id/teknologi-rekayasa-perangkat-lunak/"><i class="bi bi-globe2 "></i></a>
+                    <a href="https://www.instagram.com/trpl.polibatam/"><i class="bi bi-instagram"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </div>
+    </section>
+
+
+    <!-- ======= Pertanyaan yang sering di pertanyakaan ======= -->
+    <section id="faq" class="faq section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Pertanyaan</h2>
+          <p>Pertanyaan Umum yang sering ditanyakan?
+          </p>
+        </div>
+
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse"
+                data-bs-target="#faq-list-1"> Apa sih kegunaan website Track Your money ini ? <i
+                  class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  Jadi website ini membantu kita para mahasiswa untuk mengatur pengeluaran yang telah di keluarkan
+                  sehingga bisa mengetahui Sehemat apakah anda dalam mengeluarkan uang dalam satu bulan penuh </p>
+              </div>
+            </li>
+        </div>
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse"
+                data-bs-target="#faq-list-1"> apakah ada persyaratan khusus ketika mendaftar ke dalam website ini ? <i
+                  class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  cukup mengisi formulir pendaftar apabila pengguna baru </p>
+              </div>
+            </li>
+        </div>
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up">
+              <i class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse" class="collapse"
+                data-bs-target="#faq-list-1"> apa yang dapat saya lakukan kedalam website ini  <i
+                  class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  jadi website ini , kamu dapat mencatat pengeluaran yang telah di lakukan, menlimit pengeluaran bulanan , dan melihat pengeluaran. selain itu fitur fitur lain yang dapat mempermudahkan kamu dalam memanejemen pengeluaran</p>
+              </div>
+            </li>
+        </div>
+      </div>
+    </section>
+    <!-- ======= Div Kontak Kami ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="section-title" data-aos="fade-up">
+          <h2>Kontak</h2>
+          <p>Kontak Kami </p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Lokasi:</h4>
+                <p><a href="https://maps.app.goo.gl/a9UQyEGVEFE5hvqb7"> Jl. Ahmad Yani Batam Kota,
+                  Kota Batam, Kepulauan Riau, Indonesia.</a></p>
+              </div>
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Alamat Surel</h4>
+                <p><a href="mailto:pblprobudget.1@gmail.com">pblprobudget.1@gmail.com</a></p>
+              </div>
+              <div class="phone">
+                <i class="bi bi-phone" ></i>
+                <h4>No.Telepon</h4>
+                <p><a href="https://wa.me/6282389496939?text=Halo%20%2Capakah%20ini%20layanan%20pengaduan%20Trackyourmoney.id%3F">+62 823-8949-6939</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="200">
+          <formaction="https://formsubmit.co/aldyjhonatanhutasoit.1@gmail.com" method="POST" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Masukan Nama Lengkap " required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Masukan alamat Surel" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="10" placeholder="Isi Permasalahan yang dialami" required></textarea>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!--  Javascript  -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Javascript Tambahan -->
+  <script src="assets/js/main.js"></script>
+
+  <!-- tambahan script -->
+  <script>
+    document.onkeydown = function (e) {
+      if (event.keyCode == 123) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'F'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)) {
+        return false;
+      }
+    }
+  </script>
+  <script>
+    /* To Disable Inspect Element */
+    $(document).bind("contextmenu",function(e) {
+     e.preventDefault();
+    });
+    
+    $(document).keydown(function(e){
+        if(e.which === 123){
+           return false;
+        }
+    });
+    </script>
+
+</body>
+
+</html>
